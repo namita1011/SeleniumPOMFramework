4023ee11ab8cfbe4574671e3a3568385e6d78a3a
--namita-- <--namita.bhor@gmail.com-->
2021-03-14 22:16:21 +0530

    chnage title

--- src/main/java/com/qa/opencart/utils/Constants.java
+++ src/main/java/com/qa/opencart/utils/Constants.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Constants {
 	
 	public static final String LOGIN_PAGE_TITLE ="Account Login";
-	public static final String ACCOUNT_PAGE_TITLE="My Accounts";
+	public static final String ACCOUNT_PAGE_TITLE="My Account";
 	public static final int TOTAL_HEADERS_COUNTS=4;
 	public static final int PRODUCT_DETAILS_IMAGES=4;
 	public static final String REGISTARTION_SUCCESS_MSG ="Your Account Has Been Created!";
